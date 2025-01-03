b76d1f0645a3d18ec5e1581b616d4003e303242a
franklili3 <348104201@qq.com>
2025-01-03 20:49:53 +0800

    add transactions

--- analysis_bitcoin_srategy_backtest.py
+++ analysis_bitcoin_srategy_backtest.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 bitcoin_strategy_backtest = pd.read_csv('bitcoin_strategy_backtest.csv', index_col=0, parse_dates=True)
 returns = bitcoin_strategy_backtest['returns']
 print(returns.head())
-pf.create_returns_tear_sheet(returns)
-plt.show()
+#pf.create_returns_tear_sheet(returns)
+#plt.show()
 # %%
 import json
 bitcoin_strategy_backtest['positions'] = bitcoin_strategy_backtest['positions'].apply(lambda x: x.replace("'", '"'))
@@ -27,8 +27,15 @@ for index, row in positions.iterrows():
     positions_edited.loc[index, 'cash'] = int(row['ending_cash'])
 print(positions_edited.head()) 
 # %%
+transactions = bitcoin_strategy_backtest[['transactions']]
+transactions_edited = pd.DataFrame(0, index = transactions.index, columns = [['txn_volume', 'txn_shares']])
+for index, row in transactions.iterrows():
+    transactions_data = json.loads(row['transactions'])
 
+    if transactions_data:
+        transactions_edited.loc[index, 'txn_volume'] = transactions_data[0]['transaction_money']   
+        transactions_edited.loc[index, 'txn_shares'] = transactions_data[0]['amount']   
 #print(returns.head())
 #print(positions.head())
-#print(transactions.head())
+print(transactions.head())
 # %%
